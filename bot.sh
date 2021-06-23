d63d0249f8858a1b7914499f6bee71c4c8f860be
TalkingPanda0 <memretwitch3626@gmail.com>
2021-06-23 19:23:30 +0300

    fixed bot

--- bot.sh
+++ bot.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
-rank=0  
 user=76561198800357802
 getsuffix () {
-	[ $(echo $1 | rev | cut -c 2 | rev) -eq 1 ] && echo "suffix" && return
+	[ $(echo $1 | rev | cut -c 2 | rev) -eq 1 ] && echo "th" && return
 	case $(echo $1 | rev | cut -c 1 | rev ) in
 		1)echo "st";;
 		2)echo "nd";;
 		3)echo "rd";;
-		[4-9])echo "suffix";;
+		[4-9])echo "th";;
 	esac
 }
 while :; do
-	orank=$rank ; rank=$(curl -s https://scoresaber.com/u/$user | grep "\"Player Ranking:" | cut -c 30-) ; suffix=$(getsuffix $rank)
-	[ "$orank" != "$rank" ] && dunstify "he is ${rank}$suffix now. change title" && echo "!settitle hello 👋 it is me 😊 suffixe ${rank}$suffix best beat saber player 😎 please come watch my stream 🥺" | xclip -sel clip
+	orank=$rank 
+	rank=$(curl -s https://scoresaber.com/u/$user | grep "\"Player Ranking:" | cut -c 30- )
+	rank=${rank}$(getsuffix $rank) 
+	[ "$orank" != "$rank" ] && (dunstify "he is ${rank} now. copied the command" && echo "!settitle hello 👋 it is me 😊 the ${rank} best beat saber player 😎 please come watch my stream 🥺" | xclip -sel clip)
 done
 
