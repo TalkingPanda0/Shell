d48108a00ac36c9d4d55d3a6e68c0d6c72b568ca
TalkingPanda0 <memretwitch3626@gmail.com>
2021-06-23 04:06:02 +0300

    remove .cache

--- bot.sh
+++ bot.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-rank=0
-while :
-do
-	orank=$rank
-	rank=$(curl -s https://scoresaber.com/u/76561198800357802 | grep "\"Player Ranking:" | cut -c 30-)
+rank=0 ; user=76561198800357802
+while :; do
+	orank=$rank ; rank=$(curl -s https://scoresaber.com/u/$user | grep "\"Player Ranking:" | cut -c 30-)
 	[ "$orank" != "$rank" ] && dunstify "he is "$rank"th now change title" && echo "!settitle hello 👋 it is me 😊 the ${rank}th best beat saber player 😎 please come watch my stream 🥺" | xclip -sel clip
 	sleep 1
 done
