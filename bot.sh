70f6f42def4ba19a37ed67dfd71699df905c93c5
TalkingPanda0 <memretwitch3626@gmail.com>
2021-06-23 13:21:51 +0300

    suffixes

--- bot.sh
+++ bot.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 rank=0  
 user=76561198800357802
-th () {
-	[ $(echo $1 | rev | cut -c 2 | rev) -eq 1 ] && echo "th" && return
+getsuffix () {
+	[ $(echo $1 | rev | cut -c 2 | rev) -eq 1 ] && echo "suffix" && return
 	case $(echo $1 | rev | cut -c 1 | rev ) in
 		1)echo "st";;
 		2)echo "nd";;
 		3)echo "rd";;
-		[4-9])echo "th";;
+		[4-9])echo "suffix";;
 	esac
 }
 while :; do
-	orank=$rank ; rank=$(curl -s https://scoresaber.com/u/$user | grep "\"Player Ranking:" | cut -c 30-)
-	[ "$orank" != "$rank" ] && dunstify "he is ${rank}$(th $rank) now change title" && echo "!settitle hello 👋 it is me 😊 the ${rank}$(th $rank ) best beat saber player 😎 please come watch my stream 🥺" | xclip -sel clip
+	orank=$rank ; rank=$(curl -s https://scoresaber.com/u/$user | grep "\"Player Ranking:" | cut -c 30-) ; suffix=$(getsuffix $rank)
+	[ "$orank" != "$rank" ] && dunstify "he is ${rank}$suffix now. change title" && echo "!settitle hello 👋 it is me 😊 suffixe ${rank}$suffix best beat saber player 😎 please come watch my stream 🥺" | xclip -sel clip
 done
 
