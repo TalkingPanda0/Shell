f79d9856555ada9d137b5286a61d8656f5aeb484
TalkingPanda0 <69118613+TalkingPanda0@users.noreply.github.com>
2021-06-27 11:26:24 +0300

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -10,9 +10,8 @@ getsuffix () {
 	esac
 }
 while :; do
-	orank=$rank 
 	rank=$(curl -s https://scoresaber.com/u/$user | grep "\"Player Ranking:" | cut -c 30- )
 	rank=${rank}$(getsuffix $rank) 
-	[ "$orank" != "$rank" ] && (dunstify "he is ${rank} now. copied the command" && echo "!settitle hello 👋 it is me 😊 the ${rank} best beat saber player 😎 please come watch my stream 🥺" | xclip -sel clip)
+	[ "$orank" != "$rank" ] && (orank=$rank && dunstify "he is ${rank} now. copied the command" &&
+	echo "!settitle hello 👋 it is me 😊 the ${rank} best beat saber player 😎 please come watch my stream 🥺" | xclip -sel clip)
 done
-
