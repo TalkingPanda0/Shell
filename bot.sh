866af4dc8762b4db608118c36133e3d74e7ff177
TalkingPanda0 <memretwitch3626@gmail.com>
2021-07-03 13:47:39 +0300

    fix bot.sh

--- bot.sh
+++ bot.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+rm -f /tmp/rank
 user=76561198800357802
 getsuffix () {
 	[ "$(echo $1 | rev | cut -c 2 | rev)" = "1" ] && echo "th" && return
@@ -10,8 +11,7 @@ getsuffix () {
 	esac
 }
 while :; do
-	rank=$(curl -s https://scoresaber.com/u/$user | grep "\"Player Ranking:" | cut -c 30- )
-	 ! [ -z "$rank" ] && [ "$orank" != "$rank" ] && orank=$rank && rank=${rank}$(getsuffix $rank) && dunstify "he is ${rank} now. copied the command" &&
-	echo "!settitle hello 👋 it is me 😊 the ${rank} best beat saber player 😎 please come watch my stream 🥺" | xclip -sel clip
+rank=$(curl https://scoresaber.com/u/$user)  -o /tmp/rank 2> /tmp/curl.log && grep "\"Player Ranking:" /tmp/rank | cut -c 30- || echo " Couldn't get rank" )
+	! [ -z $rank ] && [ "$orank" != "$rank" ] && orank=$rank && rank=${rank}$(getsuffix $rank) && dunstify "he is ${rank} now. copied the command" &&
+	echo "!settitle hello 👋 it is me 😊 the ${rank} best beat saber player 😎 please come watch my stream 🥺" | xclip -sel clip 
 done
-! [ -z "$var" ] && echo "a"
