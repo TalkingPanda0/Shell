c4cef04948f37eb176939ca5f098ca6c66f9ae52
TalkingPanda0 <memretwitch3626@gmail.com>
2021-07-08 09:32:33 +0300

    read

--- resetbatstats.sh
+++ resetbatstats.sh
@@ -1,6 +1,9 @@
 #!/system/bin/sh
 echo 80 > /sys/module/autocut_charger/parameters/maximum_charge_percentage
+read $ostate < /sys/class/power_supply/battery/status
+echo $ostate
 while :; do
-	state=$(cat /sys/class/power_supply/battery/status)
-	[ "$ostate" != "$state" ] && ostate=$state && dumpsys batterystats --reset < /dev/null > /dev/null 2>&1  && echo "reset"
+echo $ostate
+	read $state < /sys/class/power_supply/battery/status
+	[ "$ostate" != "$state" ] && ostate=$state && dumpsys batterystats --reset < /dev/null > /dev/null 2>&1 
 done
